f69bbe7d38d97d55c3d3d3638517fd639a54c631
Marko Manninen <mmstud@gmail.com>
2015-01-07 12:27:22 +0200

    Added initial functionality
    Dependance on modified version of romanize module is kept, but
    isopsephy and gematria modules are not needed anymore, because Abnum is
    meant to be a general purpose package for letter value calculation.

---
+++ romanize/main.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python
+# -*- coding: utf-8 -*-
+# file: main.py
+
+"""
+ara, arabic
+cop, coptic
+eng, english
+grc, ancient greek
+heb, hebrew
+san, sanskrit
+"""
+
+import ara, cop, eng, grc, heb, san
